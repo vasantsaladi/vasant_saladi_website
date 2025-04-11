035a14841bb67fd23093e997c50ca4f2076036fe
Vasant Saladi <60614334+vasantsaladi@users.noreply.github.com>
2025-04-11 14:35:34 -0400

    Update hero-section.tsx

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -77,7 +77,7 @@ export function HeroSection() {
                 tooltip: "See my hackathon wins",
               },
               {
-                href: "https://vasantsaladiresume.tiiny.site",
+                href: "", //https://vasantsaladiresume.tiiny.site
                 icon: FileText,
                 label: "Resume",
                 tooltip: "View my resume",
